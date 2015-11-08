53b06ae1de309aa1c5df6aa334fbd96f684ce5b5
Stefan Rakel <stefan.rakel@googlemail.com>
2015-11-08 19:16:52 +0100

    Add Module loading

---
+++ SpyDashModules/FritzDECTModule.py
@@ -0,0 +1,12 @@
+
+
+class FritzDECTModule(object):
+    def __init__(self, server):
+        self.server = server
+        pass
+
+    def run(self):
+        pass
+
+    def dispatch(self, msg):
+        pass
--- server.py
+++ server.py
@@ -1,5 +1,8 @@
 from autobahn.asyncio.websocket import WebSocketServerProtocol, WebSocketServerFactory
+import importlib
 import json
+import SpyDashModules
+
 
 class SpyDashServer(object):
     """
@@ -8,6 +11,18 @@ class SpyDashServer(object):
     This class encapsules all the functionality for the SpyDash server.
     It loads available modules and handles communication over Websockets
     """
+    def __init__(self):
+        """
+        Find and load modules in the modules package
+        """
+        self.modules = []
+        config = json.load(open("config"))
+        importlib.invalidate_caches()
+        for m in config["modules"]:
+            mClass = getattr(importlib.import_module("." + m, "SpyDashModules"), m)
+            if hasattr(mClass, "run") and hasattr(mClass, "dispatch"):
+                self.modules.append(mClass(self))
+
     def dispatch(self, msg):
         """
         Dispatch received message to applicable module or handle directly
@@ -17,14 +32,13 @@ class SpyDashServer(object):
         pass
 
 
-
-
 class SpyDashServerProtocol(WebSocketServerProtocol):
     """
     The websocket protocoll for communication with clients
     """
 
     def __init__(self, spyDashServer):
+        super().__init__()
         self.server = spyDashServer
 
     def onOpen(self):
@@ -36,12 +50,13 @@ class SpyDashServerProtocol(WebSocketServerProtocol):
     def onMessage(self, payload, isBinary):
         self.server.dispatch(json.loads(payload.decode('utf-8')))
 
+
 class SpyDashServerFactory(WebSocketServerFactory):
     """
     The websocket server factory to enable broadcasting
     """
     def __init__(self, url=None):
-        WebSocketServerFactory.__init__(self, url)
+        super().__init__(self, url)
         self.clients = []
 
     def register(self, client):
@@ -61,3 +76,4 @@ class SpyDashServerFactory(WebSocketServerFactory):
         print("Message sent")
 
 if __name__ == "__main__":
+    b = SpyDashServer()
